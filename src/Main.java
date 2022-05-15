4ca1e676c074a35f7ce30736c04e6f6057558a4f
Martin-GD <gmd20172020@163.com>
2022-05-15 16:15:59 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 package project2;
 
 import java.sql.SQLException;
@@ -26,3 +27,31 @@ public class Main {
         dm.closeDB();
     }
 }
+=======
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+        check dm = new check();
+        dataImport imp = new dataImport();
+        imp.getConnection();
+//        imp.impCenter();
+//        imp.impEnterprise();
+//        imp.impModel();
+//        imp.impStaff();
+        imp.setStockIn();
+        imp.placeOrder();
+        imp.updateOrder();
+        imp.deleteOrder();
+        imp.closeConnection();
+        dm.openDB();
+        System.out.println(dm.getAllStaffCount());
+        System.out.println(dm.getContractCount());
+        System.out.println(dm.getOrderCount());
+        System.out.println(dm.getNeverSoldProductCount());
+        System.out.println(dm.getFavoriteProductModel());
+        System.out.println(dm.getAvgStockByCenter());
+        dm.closeDB();
+    }
+}
+>>>>>>> Stashed changes
