5f60404db2832a387dc14a9e27b789d93a7796b2
Martin-GD <gmd20172020@163.com>
2022-05-24 14:24:58 +0800

    5.24 ：14:25
    update web

---
+++ cs307/src/main/java/com/server/Cs307Application.java
@@ -0,0 +1,13 @@
+package com.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs307Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cs307Application.class, args);
+    }
+
+}
