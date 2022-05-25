2d0c7f2767aac60da4f7df256a961e2a77d92328
GMD <gmd20172020@163.com>
2022-05-25 11:10:05 +0800

    fileAPI
    four

--- src/FileAPI.java
+++ src/FileAPI.java
@@ -55,7 +55,7 @@ public class FileAPI {
             }
         }
     }
-
+//TODO: ENTERPRISE
     public void getEnterprise(){
         try{
             CsvReader infile = new CsvReader("enterprise.csv",',', Charset.forName("UTF-8"));
@@ -105,6 +105,163 @@ public class FileAPI {
         return false;
     }
 
+//TODO: CENTER
+    public void getCenter(){
+        try{
+            CsvReader infile = new CsvReader("center.csv",',', Charset.forName("UTF-8"));
+            String[] values;
+            infile.readHeaders();
+            while (infile.readRecord()){
+                values=infile.getValues();
+                centerM.put(values[1],new center(values));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void writeBackCenter(){
+        try {
+            BufferedWriter w = new BufferedWriter (new OutputStreamWriter(new FileOutputStream("center.csv"),"UTF-8"));
+            w.write("id,name");
+            w.newLine();
+            for (center a:centerM.values()){
+                w.write(a.getCenter());
+                w.newLine();
+            }
+            w.close();
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean addCenter(String name){
+        if (centerM.containsKey(name)){
+            return false;
+        }else {
+            centerM.put(name,new center(name));
+            return true;
+        }
+    }
+
+    public boolean deleteCenter(String name){
+        if (centerM.containsKey(name)){
+            centerM.remove(name);
+            return true;
+        }else return false;
+    }
+    public boolean updateCenter(String name){
+        if (centerM.containsKey(name)){
+            centerM.get(name).setCenter(name);
+            return true;
+        }
+        return false;
+    }
+//TODO: STAFF
+    public void getStaff(){
+        try{
+            CsvReader infile = new CsvReader("staff.csv",',', Charset.forName("UTF-8"));
+            String[] values;
+            infile.readHeaders();
+            while (infile.readRecord()){
+                values=infile.getValues();
+                staffM.put(values[4],new staff(values));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void writeBackStaff(){
+        try {
+            BufferedWriter w = new BufferedWriter (new OutputStreamWriter(new FileOutputStream("staff.csv"),"UTF-8"));
+            w.write("id,name,age,gender,number,supply_center,mobile_number,type");
+            w.newLine();
+            for (staff a:staffM.values()){
+                w.write(a.getStaff());
+                w.newLine();
+            }
+            w.close();
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean addStaff(String name,String age, String gender,String number, String supply_center,String phone,String type){
+        if (staffM.containsKey(number)){
+            return false;
+        }else {
+            staffM.put(number,new staff(name,age,gender,number,supply_center,phone,type));
+            return true;
+        }
+    }
+
+    public boolean deleteStaff(String number){
+        if (staffM.containsKey(number)){
+            staffM.remove(number);
+            return true;
+        }else return false;
+    }
+    public boolean updateStaff(String name,String age, String gender,String number, String supply_center,String phone,String type){
+        if (staffM.containsKey(number)){
+            staffM.get(number).setStaff(name, age, gender, number, supply_center, phone, type);
+            return true;
+        }
+        return false;
+    }
+//TODO: MODEL
+    public void getModel(){
+        try{
+            CsvReader infile = new CsvReader("model.csv",',', Charset.forName("UTF-8"));
+            String[] values;
+            infile.readHeaders();
+            while (infile.readRecord()){
+                values=infile.getValues();
+                modelM.put(values[2],new model(values));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void writeBackModel(){
+        try {
+            BufferedWriter w = new BufferedWriter (new OutputStreamWriter(new FileOutputStream("model.csv"),"UTF-8"));
+            w.write("id,number,model,name,unit_price");
+            w.newLine();
+            for (model a:modelM.values()){
+                w.write(a.getModel());
+                w.newLine();
+            }
+            w.close();
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean addModel(String number, String model,String name,int price){
+        if (modelM.containsKey(model)){
+            return false;
+        }else {
+            modelM.put(model,new model(number,model,name,price));
+            return true;
+        }
+    }
+
+    public boolean deleteModel(String model){
+        if (modelM.containsKey(model)){
+            modelM.remove(model);
+            return true;
+        }else return false;
+    }
+    public boolean updateModel(String number,String model, String p_name, int unit_price){
+        if (modelM.containsKey(model)){
+            modelM.get(model).setModel(number, model, p_name, unit_price);
+            return true;
+        }
+        return false;
+    }
+
 }
 class enterprise{
     int c_id;
@@ -148,14 +305,22 @@ class enterprise{
 class center{
     int sustc_id;
     String center;
-    String[] values;
+
     static int top_id = 0;
     public center(String[] values){
         this.sustc_id = Integer.parseInt(values[0]);
         this.center = values[1];
-        this.values=values;
-
         if (sustc_id>top_id) top_id=sustc_id;
+    }
+
+    public center(String center){
+        this.sustc_id = ++top_id;
+        this.center = center;
+    }
+
+    public void setCenter(String center){
+
+        this.center = center;
     }
     public String getCenter(){
         return ""+sustc_id+","+center;
@@ -179,6 +344,22 @@ class model{
 
         if (model_id>top_id) top_id=model_id;
     }
+
+    public model(String number,String model, String p_name, int unit_price){
+        this.model_id = ++top_id;
+        this.number = number;
+        this.model = model;
+        this.p_name = p_name;
+        this.unit_price = unit_price;
+    }
+
+    public void setModel(String number,String model, String p_name, int unit_price){
+
+        this.number = number;
+        this.model = model;
+        this.p_name = p_name;
+        this.unit_price = unit_price;
+    }
     public String getModel(){
         return ""+model_id+","+number+","+model+","+p_name+","+unit_price;
     }
@@ -192,9 +373,10 @@ class staff{
     String supply_center;
     String phone;
     String type;
-    String[] values;
+
     static int top_id = 0;
-    public staff(String s_id,String s_name,String gender,String age,String supply_center,String phone,String line,String[] values){
+    //String id,String name,String age, String gender,String number, String supply_center,String phone,String type
+    public staff(String[] values){
         this.id = Integer.parseInt(values[0]);
         this.name = values[1];
         this.age = values[2];
@@ -203,9 +385,30 @@ class staff{
         this.supply_center = values[5];
         this.phone = values[6];
         this.type = values[7];
-        this.values=values;
+
 
         if (id>top_id) top_id=id;
+    }
+    public staff(String name,String age, String gender,String number, String supply_center,String phone,String type){
+        this.id = ++top_id;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.number = number;
+        this.supply_center = supply_center;
+        this.phone = phone;
+        this.type = type;
+    }
+
+    public void setStaff(String name,String age, String gender,String number, String supply_center,String phone,String type){
+
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.number = number;
+        this.supply_center = supply_center;
+        this.phone = phone;
+        this.type = type;
     }
     public String getStaff(){
         return ""+id+","+name+","+age+","+gender+","+number+","+supply_center+","+phone+","+type;
