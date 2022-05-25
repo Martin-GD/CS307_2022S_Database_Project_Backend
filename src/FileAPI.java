f7f1092bb1ad47aa8c65138f795cdd11161eed69
Martin-GD <gmd20172020@163.com>
2022-05-25 15:03:08 +0800

    fileAPI change

--- src/FileAPI.java
+++ src/FileAPI.java
@@ -105,6 +105,54 @@ public class FileAPI {
         return false;
     }
 
+    public String selectEnterprise(boolean id,boolean name,boolean country,
+                                   boolean city, boolean supply_center,
+                                   boolean industry, int idC,String nameC,
+                                   String couC,String citC,String supC,String indC){
+        StringBuilder sb = new StringBuilder();
+        sb.append(id?String.format("%-4s ","id"):"");
+        sb.append(name?String.format("%-50s ","name"):"");
+        sb.append(country?String.format("%-30s ","country"):"");
+        sb.append(city?String.format("%-30s ","city"):"");
+        sb.append(supply_center?String.format("%-50s ","supply_center"):"");
+        sb.append(industry?String.format("%-40s ","industry"):"");
+        sb.append("\n");
+        for (enterprise e:enterpriseM.values()) {
+            boolean need = true;
+            if (id)
+                if (idC!=0)
+                    need = e.c_id==idC;
+
+            if (name)
+                if (!nameC.equals(""))
+                    need = e.name.equals(nameC);
+
+            if (country)
+                if (!couC.equals(""))
+                    need = e.country.equals(couC);
+            if (city)
+                if (!citC.equals(""))
+                    need = e.city.equals(citC);
+            if (supply_center)
+                if (!supC.equals(""))
+                    need = e.supply_center.equals(supC);
+            if (industry)
+                if (!indC.equals(""))
+                    need = e.industry.equals(indC);
+            if (need){
+                sb.append(id?String.format("%-4s ",e.c_id):"");
+                sb.append(name?String.format("%-50s ",e.name):"");
+                sb.append(country?String.format("%-30s ",e.country):"");
+                sb.append(city?String.format("%-30s ",e.city):"");
+                sb.append(supply_center?String.format("%-50s ",e.supply_center):"");
+                sb.append(industry?String.format("%-40s ",e.industry):"");
+                sb.append("\n");
+            }
+
+        }
+        return sb.toString();
+    }
+
 //TODO: CENTER
     public void getCenter(){
         try{
@@ -157,6 +205,33 @@ public class FileAPI {
         }
         return false;
     }
+
+    public String selectCenter(boolean id,boolean name, int idC,String nameC){
+        StringBuilder sb = new StringBuilder();
+        sb.append(id?String.format("%-4s ","id"):"");
+        sb.append(name?String.format("%-50s ","name"):"");
+        sb.append("\n");
+        for (center e:centerM.values()) {
+            boolean need = true;
+            if (id)
+                if (idC!=0)
+                    need = e.sustc_id==idC;
+
+            if (name)
+                if (!nameC.equals(""))
+                    need = e.center.equals(nameC);
+
+
+            if (need){
+                sb.append(id?String.format("%-4s ",e.sustc_id):"");
+                sb.append(name?String.format("%-50s ",e.center):"");
+                sb.append("\n");
+            }
+
+        }
+        return sb.toString();
+    }
+
 //TODO: STAFF
     public void getStaff(){
         try{
@@ -209,6 +284,68 @@ public class FileAPI {
         }
         return false;
     }
+
+    public String selectStaff(boolean id,boolean name,boolean age,
+                                   boolean gender, boolean number,
+                                   boolean supply_center, boolean phone,boolean type, int idC,String nameC,
+                                   String ageC,String genC,String numC,String supC, String phoC, String typeC){
+        StringBuilder sb = new StringBuilder();
+        sb.append(id?String.format("%-4s ","id"):"");
+        sb.append(name?String.format("%-50s ","name"):"");
+        sb.append(age?String.format("%-4s ","age"):"");
+        sb.append(gender?String.format("%-7s ","gender"):"");
+        sb.append(number?String.format("%-20s ","number"):"");
+        sb.append(supply_center?String.format("%-50s ","supply_center"):"");
+        sb.append(phone?String.format("%-15s ","phone"):"");
+        sb.append(type?String.format("%-30s ","type"):"");
+        sb.append("\n");
+        for (staff e:staffM.values()) {
+            boolean need = true;
+            if (id)
+                if (idC!=0)
+                    need = e.id==idC;
+
+            if (name)
+                if (!nameC.equals(""))
+                    need = e.name.equals(nameC);
+
+            if (age)
+                if (!ageC.equals(""))
+                    need = e.age.equals(ageC);
+            if (gender)
+                if (!genC.equals(""))
+                    need = e.gender.equals(genC);
+
+            if (number)
+                if (!numC.equals(""))
+                    need = e.number.equals(numC);
+
+            if (supply_center)
+                if (!supC.equals(""))
+                    need = e.supply_center.equals(supC);
+            if (phone)
+                if (!phoC.equals(""))
+                    need = e.phone.equals(phoC);
+            if (type)
+                if (!typeC.equals(""))
+                    need = e.type.equals(typeC);
+            if (need){
+                sb.append(id?String.format("%-4s ",e.id):"");
+                sb.append(name?String.format("%-20s ",e.name):"");
+                sb.append(age?String.format("%-4s ",e.age):"");
+                sb.append(gender?String.format("%-4s ",e.gender):"");
+                sb.append(number?String.format("%-20s ",e.number):"");
+                sb.append(supply_center?String.format("%-50s ",e.supply_center):"");
+                sb.append(phone?String.format("%-15s ",e.phone):"");
+                sb.append(type?String.format("%-20s ",e.type):"");
+                sb.append("\n");
+            }
+
+        }
+        return sb.toString();
+    }
+
+
 //TODO: MODEL
     public void getModel(){
         try{
@@ -260,6 +397,55 @@ public class FileAPI {
             return true;
         }
         return false;
+    }
+
+
+    public String selectModel(boolean id,boolean number,boolean model,
+                              boolean p_name, boolean unit_price,
+                              int idC,String numC,
+                              String modC,String pnaC,int unitC){
+        StringBuilder sb = new StringBuilder();
+        sb.append(id?String.format("%-4s ","id"):"");
+        sb.append(number?String.format("%-30s ","number"):"");
+        sb.append(model?String.format("%-30s ","model"):"");
+        sb.append(p_name?String.format("%-50s ","p_name"):"");
+        sb.append(unit_price?String.format("%-10s ","unit_price"):"");
+
+        sb.append("\n");
+        for (model e:modelM.values()) {
+            boolean need = true;
+            if (id)
+                if (idC!=0)
+                    need = (e.model_id==idC);
+
+            if (number)
+                if (!numC.equals(""))
+                    need = e.number.equals(numC);
+
+            if (model)
+                if (!modC.equals(""))
+                    need = e.model.equals(modC);
+            if (p_name)
+                if (!pnaC.equals(""))
+                    need = e.p_name.equals(pnaC);
+            if (unit_price)
+                if (unitC!=0)
+                    need = (e.unit_price==unitC);
+
+
+            if (need){
+                sb.append(id?String.format("%-4s ",e.model_id):"");
+                sb.append(number?String.format("%-30s ",e.number):"");
+                sb.append(model?String.format("%-30s ",e.model):"");
+                sb.append(p_name?String.format("%-50s ",e.p_name):"");
+                sb.append(number?String.format("%-10s ",e.number):"");
+                sb.append(unit_price?String.format("%-50s ",e.unit_price):"");
+
+                sb.append("\n");
+            }
+
+        }
+        return sb.toString();
     }
 
 }
